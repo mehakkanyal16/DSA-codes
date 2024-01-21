eeca1f9f2471d29f24931d30642704031986286f
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-01-21 20:34:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (13.99%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-       int n = nums.size();
-        for(int i=0; i<n-1; i++){
-            if(nums[i] > nums[i+1]){
-                return i;
-            }
-        }
-        return n-1;
+    //    int n = nums.size();
+    //     for(int i=0; i<n-1; i++){
+    //         if(nums[i] > nums[i+1]){
+    //             return i;
+    //         }
+    //     }
+    //     return n-1;
+    return max_element(nums.begin(),nums.end())-nums.begin();
     }
 };
