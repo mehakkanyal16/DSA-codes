896c6792148dc0198b3168ac065b36ae80364d06
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-02-15 10:57:41 +0530

    Time: 2 ms (70.80%) | Memory: 7.9 MB (33.18%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string res="";
-        int count=0;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(s[i]=='('){
-                count++;
-            }
-            else{
-                count--;
-            }
-              if((count==1&&s[i]=='('||count==0&&s[i]==')')){
-            continue;
-        }
-        else{
-            res=res+s[i];
-        }
+        // string res="";
+        // int count=0;
+        // int n=s.size();
+        // for(int i=0;i<n;i++){
+        //     if(s[i]=='('){
+        //         count++;
+        //     }
+        //     else{
+        //         count--;
+        //     }
+        //       if((count==1&&s[i]=='('||count==0&&s[i]==')')){
+        //     continue;
+        // }
+        // else{
+        //     res=res+s[i];
+        // }
 
+        // }
+        // return res;
+         string res;
+        int opened = 0;
+        for (char c : s) {
+            if (c == '(' && opened++ > 0) res += c;
+            if (c == ')' && opened-- > 1) res += c;
         }
         return res;
       
