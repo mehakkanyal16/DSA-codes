79532cbbd2a653b8e50bc7c646e2061ba33f4b6e
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-02-28 12:51:09 +0530

    Time: 7 ms (76.55%) | Memory: 12.8 MB (28.27%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,6 +1,31 @@
-class Solution {
-public:
-    int romanToInt(string s) {
+// class Solution {
+// public:
+//     int romanToInt(string s) {
+//         unordered_map<char,int>m;
+//         m['I']=1;
+//         m['V']=5;
+//         m['X']=10;
+//         m['L']=50;
+//         m['C']=100;
+//         m['D']=500;
+//         m['M']=1000;
+//         int ans=0;
+//         for(int i=0;i<s.length();i++){
+//             if(m[s[i]]<m[s[i+1]]){
+//                 ans=ans-m[s[i]];
+//             }
+//             else{
+//                 ans=ans+m[s[i]];
+//             }
+//         }
+//         return ans;
+//     }
+// };
+class Solution{
+    public:
+    int romanToInt(string s){
+        int i=0;
+        int value=0;
         unordered_map<char,int>m;
         m['I']=1;
         m['V']=5;
@@ -9,15 +34,17 @@ public:
         m['C']=100;
         m['D']=500;
         m['M']=1000;
-        int ans=0;
-        for(int i=0;i<s.length();i++){
-            if(m[s[i]]<m[s[i+1]]){
-                ans=ans-m[s[i]];
-            }
-            else{
-                ans=ans+m[s[i]];
-            }
+        while(i<s.length()){
+        if(i<s.length()-1&&m[s[i]]<m[s[i+1]]){
+            value=value+m[s[i+1]]-m[s[i]];
+            i=i+2;
         }
-        return ans;
+        else{
+            value=value+m[s[i]];
+            i=i+1;
+        }
+        }
+return value;
+
     }
 };
