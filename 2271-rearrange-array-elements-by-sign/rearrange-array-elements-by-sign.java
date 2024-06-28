7f0693a10d782b90fcb83a192e0a401733691086
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-06-28 22:35:59 +0530

    Time: 3 ms (100.00%) | Memory: 85.2 MB (8.71%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,26 +1,46 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int s=nums.length;
-       int []p=new int[s/2];
-       int []n=new int[s/2];
-       int pIndex=0;
-       int nIndex=0;
-       for(int i=0;i<s;i++){
+    //     int s=nums.length;
+    //    int []p=new int[s/2];
+    //    int []n=new int[s/2];
+    //    int pIndex=0;
+    //    int nIndex=0;
+    //    for(int i=0;i<s;i++){
+    //     if(nums[i]>=0){
+    //         p[pIndex++]=nums[i];
+    //     }
+    //     else if(nums[i]<0){
+    //         n[nIndex++]=nums[i];
+    //     }
+    //    }
+    //     for(int i=0;i<s;i++){
+    //         if(i%2==0){
+    //             nums[i]=p[i/2];
+    //         }
+    //         else{
+    //             nums[i]=n[i/2];
+    //         }
+    //     }
+    //     return nums;
+
+
+
+
+    int s=nums.length;
+    int []ans=new int[s];
+    int p=0;
+    int n=1;
+    for(int i=0;i<s;i++){
         if(nums[i]>=0){
-            p[pIndex++]=nums[i];
+            ans[p]=nums[i];
+            p=p+2;
         }
         else if(nums[i]<0){
-            n[nIndex++]=nums[i];
+            ans[n]=nums[i];
+            n=n+2;
         }
-       }
-        for(int i=0;i<s;i++){
-            if(i%2==0){
-                nums[i]=p[i/2];
-            }
-            else{
-                nums[i]=n[i/2];
-            }
-        }
-        return nums;
+    } 
+    return ans;
     }
+    
 }
