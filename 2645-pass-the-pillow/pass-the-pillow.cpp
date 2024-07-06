623a8983c42bc8f2848c8157cd5ffdf0571fcc03
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-07-06 12:47:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (71.18%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -2,8 +2,23 @@ class Solution {
 public:
     int passThePillow(int n, int time) {
 
-       int chunks=time/(n-1);
-       return chunks%2==0?(time%(n-1)+1):(n-time%(n-1));
+    //    int chunks=time/(n-1);
+    //    return chunks%2==0?(time%(n-1)+1):(n-time%(n-1));
+
+
+    int currentPillowPosition=1;
+    int currentTime=0;
+    int direction=1;
+    while(currentTime<time){
+        if(0<currentPillowPosition+direction && currentPillowPosition+direction<=n){
+            currentPillowPosition+=direction;
+            currentTime++;
+        }
+        else{
+            direction*=-1;
+        }
+    }
+    return currentPillowPosition;
 
     
     }
