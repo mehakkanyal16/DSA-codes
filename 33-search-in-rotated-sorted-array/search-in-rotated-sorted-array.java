5189f4c552b1292a1a9b9bd966dc370510df0238
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-07-21 00:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (85.64%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -17,22 +17,8 @@ class Solution {
         return -1;
     }
     private int pivotIndex(int nums[]){
-        // int n=nums.length;
-        // int start=1;
-        // int end=n-2;
-        // while(start<=end){
-        //     int mid=start+(end-start)/2;
-        //     if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
-        //         return mid;
-        //     }
-        //     else if(nums[mid]<nums[mid-1]){
-        //         start=mid+1;
-        //     }
-        //     else{
-        //         end=mid-1;
-        //     }
-        // }
-        // return -1;
+       
+      
        
 
        int n = nums.length;
