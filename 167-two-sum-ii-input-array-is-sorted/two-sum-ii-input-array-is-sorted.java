019e3d2ce4707527d203d64efa1b98df7c9f2d99
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-07-22 02:29:28 +0530

    Time: 4 ms (20.77%) | Memory: 47 MB (58.44%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,5 +1,23 @@
 class Solution {
+    private int binarySearch(int[] numbers,int target,int s,int e){
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(numbers[m]==target){
+                return m;
+            }
+            else if(target>numbers[m]){
+                s=m+1;
+            }
+            else{
+                e=m-1;
+            }
+        }
+        return -1;
+    }
     public int[] twoSum(int[] numbers, int target) {
+
+    //   linear search
+
         // for(int i=0;i<numbers.length;i++){
         //     for(int j=i+1;j<numbers.length;j++){
         //         if(numbers[i]+numbers[j]==target){
@@ -10,20 +28,36 @@ class Solution {
         // return new int[]{-1,-1};
 
 
-        int start=0;
-        int end=numbers.length-1;
-        while(start<end){
-            if(numbers[start]+numbers[end]==target){
-                return new int[]{start+1,end+1};
+
+        // two pointers approach
+
+        // int start=0;
+        // int end=numbers.length-1;
+        // while(start<end){
+        //     if(numbers[start]+numbers[end]==target){
+        //         return new int[]{start+1,end+1};
+        //     }
+        //     else if(numbers[start]+numbers[end]<target){
+        //         start++;
+        //     }
+        //     else{
+        //         end--;
+        //     }
+        // }
+        // return new int[]{-1,-1};
+
+        //   binary search
+
+        for(int i=0;i<numbers.length;i++){
+            int temp=target-numbers[i];
+            int index=binarySearch(numbers,temp,i+1,numbers.length-1);
+            if(index!=-1){
+                return new int[]{i+1,index+1};
             }
-            else if(numbers[start]+numbers[end]<target){
-                start++;
-            }
-            else{
-                end--;
-            }
+
         }
-        return new int[]{-1,-1};
+
+         return new int[]{-1,-1};
         
     }
 }
