dd36185d0f222941b717758d87fe3c458f2b3c20
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-07-22 02:50:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (73.83%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,14 +1,47 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if(num==1){
+
+     //  time complexity O(n);
+
+         // if(num==1){
+        //     return true;
+        // }
+        // for(int i=0;i<num;i++){
+        //     if(i*i==num){
+        //         return true;
+        //     }
+        // }
+        // return false;
+   
+//   time complexity O(sqrt(n));
+        // if(num==1){
+        //     return true;
+        // }
+        // for(int i=0;i<=Math.sqrt(num);i++){
+        //     if(i*i==num){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+    if(num<2){
+    return true;
+     }
+    int start=1;
+    int end=(int)Math.sqrt(num);
+     while(start<=end){
+       int mid=start+(end-start)/2;
+        if(mid*mid==num){
             return true;
         }
-        for(int i=0;i<=Math.sqrt(num);i++){
-            if(i*i==num){
-                return true;
-            }
+        else if(mid*mid<num){
+            start=mid+1;
         }
-        return false;
+        else{
+            end=mid-1;
+        }
+     }
+     return false;
         
     }
 }
