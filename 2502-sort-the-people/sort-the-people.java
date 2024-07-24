a5e839191e7be9337e247c52373e4fc9de37f5a3
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-07-25 02:05:13 +0530

    Time: 7 ms (91.41%) | Memory: 45.7 MB (7.26%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -8,16 +8,30 @@ class Solution {
         heights[j]=tempHeight;
     }
     public String[] sortPeople(String[] names, int[] heights) {
-          for(int i=0;i<names.length;i++){
-            int maxIndex=i;
-           for(int j=i+1;j<names.length;j++){
-                 if(heights[j]>heights[maxIndex]){
-                   maxIndex=j;
-                  }
-           }
-           swap(names,heights,i,maxIndex);
+        //   for(int i=0;i<names.length;i++){
+        //     int maxIndex=i;
+        //    for(int j=i+1;j<names.length;j++){
+        //          if(heights[j]>heights[maxIndex]){
+        //            maxIndex=j;
+        //           }
+        //    }
+        //    swap(names,heights,i,maxIndex);
+        // }
+        // return names;
+
+
+        Map<Integer , String > map = new HashMap<>();
+        for(int i=0;i<names.length;i++){
+            map.put(heights[i] , names[i]); // putting it in a hashmap
         }
-        return names;
+        Arrays.sort(heights);
+        String[] result = new String[heights.length]; //create new array of the length of hashmap
+        int index = 0;
+        for(int i=heights.length-1;i>=0;i--){ // reverse loop beacuse the result of arrays.sort will be in smaller to greater but we want greater to smaller
+            result[index]= map.get(heights[i]); //putting result in the new array
+            index++;
+        }
+        return result;
         
     }
 }
