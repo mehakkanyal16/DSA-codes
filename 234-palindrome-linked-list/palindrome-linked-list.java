59cbeb2cee09f52c25471115a4069c0c4bd115e1
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-08-02 21:55:47 +0530

    Time: 6 ms (54.04%) | Memory: 69.1 MB (11.45%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,19 +10,71 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode curr=head;
-        List<Integer>list=new ArrayList();
-        while(curr!=null){
-            list.add(curr.val);
-            curr=curr.next;
-        }
-        int left=0;
-        int right=list.size()-1;
-        while(left<right&&list.get(left)==list.get(right)){
-            left++;
-            right--;
-        }
-        return left>=right;
+        // ListNode curr=head;
+        // List<Integer>list=new ArrayList();
+        // while(curr!=null){
+        //     list.add(curr.val);
+        //     curr=curr.next;
+        // }
+        // int left=0;
+        // int right=list.size()-1;
+        // while(left<right&&list.get(left)==list.get(right)){
+        //     left++;
+        //     right--;
+        // }
+        // return left>=right;
         
+        // time and space complexity of the above code is O(n).. so thats  why we can still optimise it more:
+
+
+     ListNode mid=Mid(head);
+     ListNode headSecond=reverse(mid);
+     ListNode RereverseHead=headSecond;
+
+
+     while(head!=null&&headSecond!=null){
+        if(head.val!=headSecond.val){
+            break;
+        }
+        head=head.next;
+        headSecond=headSecond.next;
+     }
+     reverse(RereverseHead);
+     if(head==null||headSecond==null){
+        return true;
+     }
+     return false;
+
+
+
+
+    }
+
+
+    public ListNode Mid(ListNode head){
+        ListNode s=head;
+        ListNode f=head;
+        while(f!=null&&f.next!=null){
+            s=s.next;
+            f=f.next.next;
+        }
+        return s;
+    }
+    public ListNode reverse(ListNode head){
+        if(head==null){
+            return head;
+        }
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode next=curr.next;
+        while(curr!=null){
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+            if(next!=null){
+                next=next.next;
+            }
+        }
+        return prev;
     }
 }
