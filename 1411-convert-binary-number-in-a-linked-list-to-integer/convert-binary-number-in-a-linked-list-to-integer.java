472130226479f901aa8895eecf9fbe13ed0a84c7
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-08-07 02:09:19 +0530

    Time: 1 ms (15.66%) | Memory: 41.5 MB (7.47%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.java
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.java
@@ -10,24 +10,43 @@
  */
 class Solution {
     public int getDecimalValue(ListNode head) {
-        ListNode temp=head;
-        int size=0;
-        while(temp!=null){
-            size++;
-            temp=temp.next;
-        }
-        int i=size-1;
-        temp=head;
-        int ans=0;
-        while(temp!=null){
-            if(temp.val==1){
-                ans+=Math.pow(2,i);
-            }
-            i--;
-            temp=temp.next;
+        // ListNode temp=head;
+        // int size=0;
+        // while(temp!=null){
+        //     size++;
+        //     temp=temp.next;
+        // }
+        // int i=size-1;
+        // temp=head;
+        // int ans=0;
+        // while(temp!=null){
+        //     if(temp.val==1){
+        //         ans+=Math.pow(2,i);
+        //     }
+        //     i--;
+        //     temp=temp.next;
 
+        // }
+        // return ans;
+
+        if(head==null){
+            return -1;
+        }
+        Stack<Integer>st=new Stack<>();
+        while(head!=null){
+            st.push(head.val);
+            head=head.next;
+        }
+        int ans=0;
+        int i=0;
+        while(!st.empty()){
+            int num=st.pop();
+            ans+=num*Math.pow(2,i);
+            i++;
         }
         return ans;
+
+
         
     }
 }
