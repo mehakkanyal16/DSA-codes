e9c783388b08a7f15ae2d834a18f8623c57173b6
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-09-20 10:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (33.90%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int acount=0;
+        int lcount=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='A'){
+                acount++;
+                if(acount>1)return false;
+            }
+            if(s.charAt(i)=='L'){
+                lcount++;
+                if(lcount>2)return false;
+            }
+            else{
+                lcount=0;
+            }
+        }
+        return true;
+        
+    }
+}
