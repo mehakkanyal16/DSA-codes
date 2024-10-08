fb59fa50f7a0984ef4a437575fca9a42ca972095
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-10-08 13:40:42 +0530

    Time: 2 ms (19.74%) | Memory: 40.6 MB (64.61%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,26 +1,25 @@
-class Solution {
+class Solution { 
     public int findSquare(int n){
         int ans=0;
         while(n>0){
             int rem=n%10;
             ans=ans+rem*rem;
             n=n/10;
-            
         }
         return ans;
-
     }
     public boolean isHappy(int n) {
-        int slow=n;// for one step ahead
-        int fast=n;// for two steps ahead
-        do{
-            slow=findSquare(slow);
-            fast=findSquare(findSquare(fast));
-        }while(slow!=fast);
-        if(slow==1){
-            return true;
+      HashSet<Integer>set=new HashSet<>();
+      while(n!=1){
+        if(set.contains(n)){
+            return false;
+        }else{
+            set.add(n);
+            n=findSquare(n);
+            
         }
-        return false;
-        
+      
+      }
+        return true;
     }
 }
