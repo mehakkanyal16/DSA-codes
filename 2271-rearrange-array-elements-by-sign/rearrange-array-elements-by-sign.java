c09178307cabec0e96f38df9f64081ab7f63e7a0
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-11-26 20:27:32 +0530

    Time: 3 ms (100.00%) | Memory: 76.9 MB (55.72%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,46 +1,19 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-    //     int s=nums.length;
-    //    int []p=new int[s/2];
-    //    int []n=new int[s/2];
-    //    int pIndex=0;
-    //    int nIndex=0;
-    //    for(int i=0;i<s;i++){
-    //     if(nums[i]>=0){
-    //         p[pIndex++]=nums[i];
-    //     }
-    //     else if(nums[i]<0){
-    //         n[nIndex++]=nums[i];
-    //     }
-    //    }
-    //     for(int i=0;i<s;i++){
-    //         if(i%2==0){
-    //             nums[i]=p[i/2];
-    //         }
-    //         else{
-    //             nums[i]=n[i/2];
-    //         }
-    //     }
-    //     return nums;
+        int n=nums.length;
+        int[] res=new int[n];
+      int pos=0;
+      int neg=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0){
+              res[pos]=nums[i];
+              pos=pos+2;
 
-
-
-
-    int s=nums.length;
-    int []ans=new int[s];
-    int p=0;
-    int n=1;
-    for(int i=0;i<s;i++){
-        if(nums[i]>=0){
-            ans[p]=nums[i];
-            p=p+2;
+            }else{
+                res[neg]=nums[i];
+                neg=neg+2;
+            }
         }
-        else if(nums[i]<0){
-            ans[n]=nums[i];
-            n=n+2;
-        }
-    } 
-    return ans;
+    return res;
     }
-    
 }
