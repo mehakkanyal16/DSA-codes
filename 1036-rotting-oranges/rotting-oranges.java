375d9beb946d24a80b6e3a13b571544853e075c3
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-12-06 18:34:39 +0530

    Time: 2 ms (71.96%) | Memory: 43.3 MB (6.40%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -14,46 +14,52 @@ class Solution {
         int m=grid[0].length;
         Queue<pair>q=new LinkedList<>();
         int[][] vis=new int[n][m];
+
+
+        int tm=0;
         int cnt=0;
         int cntFresh=0;
-        int tm=0;
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.add(new pair(i,j,0));
-                      vis[i][j]=2;
+                    vis[i][j]=2;
                 }else{
-                      vis[i][j]=0;
+                    vis[i][j]=0;
+
                 }
-              
                 if(grid[i][j]==1){
                     cntFresh++;
                 }
             }
         }
-        int[] drow={-1,0,1,0};
-        int[] dcol={0,1,0,-1};
+     
+
         while(!q.isEmpty()){
             int r=q.peek().row;
             int c=q.peek().col;
             int t=q.peek().t;
             tm=Math.max(tm,t);
             q.remove();
+            int[] drow={-1,0,1,0};
+            int[] dcol={0,1,0,-1};
             for(int i=0;i<4;i++){
-            int nrow=r+drow[i];
-            int ncol=c+dcol[i];
-            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&grid[nrow][ncol]==1&&vis[nrow][ncol]==0){
-                q.add(new pair(nrow,ncol,t+1));
-                vis[nrow][ncol]=2;
-                cnt++;
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=00&&nrow<n&&ncol>=0&&ncol<m&&grid[nrow][ncol]==1&&vis[nrow][ncol]==0){
+                    q.add(new pair(nrow,ncol,t+1));
+                    vis[nrow][ncol]=1;
+                    cnt++;
+
+                }
+
             }
-          }
         }
         if(cnt!=cntFresh){
             return -1;
         }
         return tm;
 
-        
     }
 }
