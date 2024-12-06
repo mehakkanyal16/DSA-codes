1c9dfe0f043d68ba246f95ee264bfe986534edcb
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-12-06 18:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (57.34%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,24 +1,30 @@
 class Solution {
-    public void dfs(int row,int col,int[][]ans,int[][]image,int iniColor,int color,int[] delrow,int[] delcol){
-        ans[row][col]=color;
-        int n=image.length;
-        int m=image[0].length;
-        for(int i=0;i<4;i++)
-        {
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&image[nrow][ncol]==iniColor&&ans[nrow][ncol]!=color){
-                dfs(nrow,ncol,ans,image,iniColor,color,delrow,delcol);
-            }
+    public void dfs(int row,int col,int[][]image,int[][]ans,int iniColor,int color,int[] drow,int[] dcol){
 
+     ans[row][col]=color;
+      int n=image.length;
+        int m=image[0].length;
+        for(int i=0;i<4;i++){
+            int nrow=row+drow[i];
+            int ncol=col+dcol[i];
+            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&image[nrow][ncol]==iniColor&&ans[nrow][ncol]!=color){
+                dfs(nrow,ncol,image,ans,iniColor,color,drow,dcol);
+            }
         }
+      
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int iniColor=image[sr][sc];
-        int[][] ans=image;
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,-1,0,1};
-        dfs(sr,sc,ans,image,iniColor,color,delrow,delcol);
-        return ans;
+
+        int n=image.length;
+        int m=image[0].length;
+
+        int[][]ans=image;
+
+        int[] drow={-1,0,1,0};
+        int[] dcol={0,1,0,-1};
+        dfs(sr,sc,image,ans,iniColor,color,drow,dcol);
+
+       return ans;
     }
 }
