3067530230841a6356caede8d761ad37dcedef57
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-12-17 20:06:05 +0530

    Time: 6 ms (55.98%) | Memory: 46.6 MB (6.89%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[] findOrder(int n, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        int m=prerequisites.length;
+         for(int i=0;i<m;i++){
+             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+         }
+         
+         int indegree[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+
+        Queue<Integer> q = new LinkedList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+
+        int topo[] = new int[n];
+        int ind = 0;
+        // o(v + e)
+        while (!q.isEmpty()) {
+            int node = q.peek();
+
+            q.remove();
+            topo[ind++] = node;
+            // node is in your topo sort
+            // so please remove it from the indegree
+
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.add(it);
+            }
+        }
+
+
+        if (ind == n) return topo;
+        int[] arr = {};
+        return arr;
+    }
+}
