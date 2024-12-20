844aedb89e7323e6d6ac28ad58a62c8bf883d162
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2024-12-20 19:25:16 +0530

    Time: 61 ms (49.68%) | Memory: 62 MB (6.75%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,35 +1,15 @@
 class Solution {
-    public void heapify(int[] arr,int n,int i){
-        int largest=i;
-        int left=2*i+1;
-        int right=2*i+2;
-        if(left<n&&arr[left]>arr[largest]){
-            largest=left;
-        }
-        if(right<n&&arr[right]>arr[largest]){
-            largest=right;
-        }
-        if(largest!=i){
-            int temp=arr[i];
-            arr[i]=arr[largest];
-            arr[largest]=temp;
-            heapify(arr,n,largest);
-        }
-    }
     public int findKthLargest(int[] nums, int k) {
         int n=nums.length;
-         for(int i=n/2-1;i>=0;i--){
-            heapify(nums,n,i);
-         }
-         for(int i=n-1;i>0;i--){
-            int temp=nums[0];
-            nums[0]=nums[i];
-            nums[i]=temp;
-            heapify(nums,i,0);
-         }
-         return nums[n-k];
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            pq.add(nums[i]);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
 
-
-
+        
     }
 }
