d6a8aa5920ccc5a0cf4f26478ee28be0fbbc214e
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2025-01-22 13:44:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (51.42%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,68 +1,61 @@
 class Solution {
-    private int binarySearch(int[] nums,int target,int start,int end){
+    public int binarySearch(int[] nums,int start,int end,int target){
+       
         while(start<=end){
-            int mid=start+(end-start)/2;
+             int mid=start+(end-start)/2;
             if(nums[mid]==target){
                 return mid;
-            }
-            else if(target>nums[mid]){
+            }else if(target>nums[mid]){
                 start=mid+1;
-
-            }
-            else{
+            }else{
                 end=mid-1;
             }
-            
         }
         return -1;
-    }
-    private int pivotIndex(int nums[]){
-       
-      
-       
 
-       int n = nums.length;
-        int start = 0;
-        int end = n - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (mid < end && nums[mid] > nums[mid + 1]) {
+    }
+    public int pivotIndex(int[] nums){
+        int start=0;
+        int end=nums.length-1;
+        if (nums[start] <= nums[end]) {
+            return -1;
+        }
+
+      
+        while(start<=end){
+             int mid=start+(end-start)/2;
+            if(mid<end&&nums[mid]>nums[mid+1]){
                 return mid;
             }
-            if (mid > start && nums[mid] < nums[mid - 1]) {
-                return mid - 1;
+            else if(mid>start&&nums[mid]<nums[mid-1]){
+                return mid-1;
             }
-            if (nums[start] >= nums[mid]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+            else if(nums[start]<=nums[mid]){
+                start=mid+1;
+            }else{
+                end=mid-1;
             }
         }
-        return -1;
-    }
+      return -1;
 
+    }
     public int search(int[] nums, int target) {
-        int n=nums.length;
+         int n=nums.length;
          if (n == 1) {
             return nums[0] == target ? 0 : -1;
         }
+    
         int pivot=pivotIndex(nums);
         if(pivot==-1){
-            return binarySearch(nums,target,0,n-1);
+            return binarySearch(nums,0,n-1,target);
         }
         if(nums[pivot]==target){
             return pivot;
         }
-        if(target>=nums[0]){
-            return binarySearch(nums,target,0,pivot-1);
-
+        else if(target>=nums[0]){
+            return binarySearch(nums,0,pivot-1,target);
         }
-        
-        return binarySearch(nums,target,pivot+1,n-1);
-        
-    }    
+            return binarySearch(nums,pivot+1,nums.length-1,target);
 
-}   
-
-        
-    
+    }
+}
