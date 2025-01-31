ea29af8e770f50245d4285c5b47174267bc5ba19
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2025-01-31 14:23:59 +0530

    Time: 2 ms (11.68%) | Memory: 45 MB (39.36%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,40 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-    //     if(nums.length==0){
-    //         return 0;
-    //     }
-    //    int indx=1;
-    //    for(int i=1;i<nums.length;i++){
-    //       if(nums[i]!=nums[i-1]){
-    //         nums[indx]=nums[i];
-    //         indx++;
-    //       }
-    //    }
-    //    return indx;
-    // ArrayList<Integer>list=new ArrayList<>();
-    // list.add(nums[0]);
-    // for(int i=1;i<nums.length;i++){
-    //     if(nums[i]!=nums[i-1]){
-    //         list.add(nums[i]);
-    //     }
-    // }
-    // for(int i=0;i<list.size();i++){
-    //     nums[i]=list.get(i);
-    // }
-    // return list.size();
-
-
-    HashSet<Integer>set=new HashSet<>();
-    int write=0;
-    for(int i=0;i<nums.length;i++){
-       if(!set.contains(nums[i])){
-        set.add(nums[i]);
-        nums[write++]=nums[i];
-
-       }
-
-    }
-    return write;
+        HashSet<Integer>hs=new HashSet<>();
+        int write=0;
+        for(int i=0;i<nums.length;i++){
+            if(!hs.contains(nums[i])){
+                hs.add(nums[i]);
+            
+                nums[write++]=nums[i];
+            }
+        }
+        return write;
         
     }
 }
