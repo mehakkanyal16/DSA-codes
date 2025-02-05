79dd82cebb35d7ef41a2933f38037a5ae6ce0d14
mehakkanyal16 <149966478+mehakkanyal16@users.noreply.github.com>
2025-02-05 21:15:34 +0530

    Time: 1 ms (52.21%) | Memory: 41.9 MB (15.64%) - LeetSync

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        if(s1==s2){
+            return true;
+        }
+        int[] freq1=new int[26];
+        int[] freq2=new int[26];
+        int numdiff=0;
+        for(int i=0;i<s1.length();i++){
+            char c1=s1.charAt(i);
+            char c2=s2.charAt(i);
+            if(c1!=c2){
+                numdiff++;
+                if(numdiff>2){
+                return false;
+             }
+            }
+           
+            freq1[c1-'a']++;
+            freq2[c2-'a']++;
+        }
+        return Arrays.equals(freq1,freq2);
+
+
+    }
+}
