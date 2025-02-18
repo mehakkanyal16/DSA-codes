329b410c2dd0a21b40842595e17c2e39cd42ccdc
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-02-18 22:11:56 +0530

    Time: 8 ms (76.59%) | Memory: 43.5 MB (91.97%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,24 +1,23 @@
 class Solution {
     public int calculate(String s) {
         int res=0;
-        int num = 0;//used to convert number to digit;
-        int sign = 1; 
+        int num=0;
+        int sign=1;
         Stack<Integer>st=new Stack<>();
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
-      
             if(Character.isDigit(c)){
                 num=num*10+(c-'0');
-                
 
             }else if(c=='+'){
-                res+=sign*num;
+                res+=num*sign;
                 num=0;
                 sign=1;
-            }else if(c=='-'){
-                res+=sign*num;
+            }
+            else if(c=='-'){
+                res+=num*sign;
                 num=0;
-                sign=-1; 
+                sign=-1;
             }else if(c=='('){
                 st.push(res);
                 st.push(sign);
@@ -26,20 +25,16 @@ class Solution {
                 sign=1;
 
             }else if(c==')'){
-                res += sign * num;
-                num = 0;
-                res *= st.pop(); // Pop the sign
-                res += st.pop(); // Pop the result
+                res+=num*sign;
+                num=0;
+                res*=st.pop();
+                res+=st.pop();
 
-
+                
             }
-     
-   
-
         }
-         res+=sign*num;
+        res+=num*sign;
         return res;
-
         
     }
 }
