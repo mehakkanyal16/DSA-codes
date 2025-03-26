a9b4abfb9229f76b56cbbd294a535aaab9875b1a
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-03-27 01:16:36 +0530

    Time: 14 ms (53.30%) | Memory: 44.6 MB (67.18%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,12 +1,16 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int n=s.length();
-        int count=0;
-        int[] lastSeen={-1,-1,-1};
-        for(int i=0;i<n;i++){
-            lastSeen[s.charAt(i)-'a']=i;
-            if(lastSeen[0]!=-1&&lastSeen[1]!=-1&&lastSeen[2]!=-1){
-                  count=count+(1+Math.min(lastSeen[0],Math.min(lastSeen[1],lastSeen[2])));
+        int left = 0, count = 0;
+        int[] freq = new int[3]; // To store the frequency of 'a', 'b', 'c'
+
+        for (int right = 0; right < s.length(); right++) {
+            freq[s.charAt(right) - 'a']++; // Increment frequency of current character
+
+            // If all three characters are present in the window
+            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+                count += s.length() - right; // Count all valid substrings
+                freq[s.charAt(left) - 'a']--; // Shrink window from the left
+                left++;
             }
         }
         return count;
