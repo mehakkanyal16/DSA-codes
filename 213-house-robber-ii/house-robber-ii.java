06e845fbce0f834af946b079b2a196743cf337eb
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-04-08 16:26:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (38.26%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,39 +1,43 @@
 class Solution {
-    public int maxOfAdjacent(int[] nums){
-        int n=nums.length;
-        int prev=nums[0];
-        int prev1=0;
-        for(int i=1;i<n;i++){
+    public int maxValue(int[] nums,int ind){
+      
+         int prev2=0;
+         int prev=nums[0];
+         int n=nums.length;
+         for(int i=1;i<n;i++){
             int take=nums[i];
             if(i>1){
-                take+=prev1;
+                take=nums[i]+prev2;
             }
-            int not_take=prev;
+            int not_take=0+prev;
             int curr=Math.max(take,not_take);
-            prev1=prev;
+            prev2=prev;
             prev=curr;
-        }
-        return prev;
+
+         }
+         return prev;
     }
     public int rob(int[] nums) {
-       int n=nums.length;
-       if(n==0){
-        return 0;
-       }
-       if(n==1){
-        return nums[0];
-       }
-       int[] temp1=new int[n+1];
-       int[] temp2=new int[n+1];
-       for(int i=0;i<n;i++){
-        if(i!=0){
-            temp1[i-1]=nums[i];
+        int n=nums.length;
+        if(n==0){
+            return 0;
         }
-        if(i!=n-1){
-            temp2[i]=nums[i];
+        if(n==1){
+            return nums[0];
         }
+        int[] a1=new int[n];
+        int[] a2=new int[n];
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                a1[i-1]=nums[i];
+            }
+            if(i!=n-1){
+                a2[i]=nums[i];
+            }
+        }
+        return Math.max(maxValue(a1,n-1),maxValue(a2,n-1));
+       
 
-       }
-        return Math.max(maxOfAdjacent(temp1),maxOfAdjacent(temp2));
+        
     }
 }
