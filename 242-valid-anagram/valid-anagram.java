92245630b812b97a5f11357c089a9d7fc80d01f2
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-05-26 23:31:53 +0530

    Time: 3 ms (91.09%) | Memory: 44.5 MB (55.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,9 +2,11 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         char[] arr1=s.toCharArray();
         Arrays.sort(arr1);
+
         char[] arr2=t.toCharArray();
         Arrays.sort(arr2);
         return Arrays.equals(arr1,arr2);
+
         
     }
 }
