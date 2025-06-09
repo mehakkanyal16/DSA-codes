37a0d67259092a48a29764187808c2b7c5c83c05
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-06-10 00:41:53 +0530

    Time: 34 ms (5.23%) | Memory: 61.4 MB (6.89%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,41 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // Arrays.sort(nums);
-        // for(int i=0;i<nums.length-1;i++){
-        //     if(nums[i]==nums[i+1]){
-        //         return true;
-        //     }
-        // }
-        // return false;
-
-        if(nums.length<2){
-            return false;
-        }
-
-        // HashMap<Integer,Integer>mp=new HashMap<>();
-        // for(int i=0;i<nums.length;i++){
-        //     mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-        // }
-        // for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
-        //     if(entry.getValue()>=2){
-        //         return true;
-        //     }
-        // }
-        // return false;
-
-        HashSet<Integer>set=new HashSet<>();
+        HashMap<Integer,Integer>mp=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-           if(set.contains(nums[i])) {
-                return true;
-           }
-    
-            set.add(nums[i]);
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
-      
+        for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
+            if(entry.getValue()>=2){
+                return true;
+            }
+        }
         return false;
+
+
         
-
-
-
     }
 }
