744c6aec68e5927f13f4cdcee28ad4ad7d2b2d22
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-06-14 00:43:45 +0530

    Time: 1 ms (99.45%) | Memory: 45.5 MB (32.99%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,34 +1,16 @@
 class Solution {
-    public int n;
-    public int[] dp;
-
-    public int helper(int idx, int[] nums) {
-        if (idx >= n - 1) return 0; // Reached or passed the end
-        if (nums[idx] == 0) return Integer.MAX_VALUE;
-
-        if (dp[idx] != -1) return dp[idx]; // Memoization check
-
-        int mini = Integer.MAX_VALUE;
-
-        for (int j = 1; j <= nums[idx]; j++) {
-            int nextIdx = idx + j;
-            if (nextIdx < n) {
-                int result = helper(nextIdx, nums);
-                if (result != Integer.MAX_VALUE) {
-                    mini = Math.min(mini, 1 + result);
-                }
+    public int jump(int[] nums) {
+        int maxReach = 0;
+        int jump = 0;
+        int pos = 0;
+        for(int i = 0; i<nums.length - 1; i++){
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if(i == pos){
+                pos = maxReach;
+                jump++;
             }
         }
 
-        return dp[idx] = mini;
-    }
-
-    public int jump(int[] nums) {
-        this.n = nums.length;
-        this.dp = new int[n];
-        Arrays.fill(dp, -1);
-        
-        int ans = helper(0, nums);
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+        return jump;
     }
 }
