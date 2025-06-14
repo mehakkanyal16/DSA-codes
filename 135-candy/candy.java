c2f95c2ca03efec5ca3314f207cb8f1d5ffbd14a
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-06-15 02:17:56 +0530

    Time: 4 ms (23.98%) | Memory: 45.7 MB (68.57%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -2,7 +2,9 @@ class Solution {
     public int candy(int[] arr) {
         int n=arr.length;
         int[] left=new int[n];
+          Arrays.fill(left,1);
         int[] right=new int[n];
+          Arrays.fill(right,1);
         //left traversal 
         left[0]=1;
         for(int i=1;i<=n-1;i++){
@@ -10,23 +12,13 @@ class Solution {
             if(arr[i]>arr[i-1]){
                 left[i]=left[i-1]+1;
             }
-            else{
-                left[i]=1;
-            }
-            
-
         }
         //right traversal;
         right[n-1]=1;
         for(int i=n-2;i>=0;i--){
             if(arr[i]>arr[i+1]){
                 right[i]=right[i+1]+1;
-            }else{
-                 right[i]=1;
-
             }
-           
-
         }
         int cnt=0;
         for(int i=0;i<n;i++){
