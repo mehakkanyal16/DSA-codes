ccc67655925bded8fed5df637884b8df0edd504e
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-06-19 17:51:04 +0530

    Time: 34 ms (17.74%) | Memory: 45.3 MB (80.14%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,29 +1,27 @@
 class Solution {
-    public int f(int i,int j,String t1,String t2,int[][] dp){
-            if(i<0||j<0){
-            return 0;
-        }
+      public int solve(int i,int j,String s1,String s2 ,int[][] dp){
+        if(i<0||j<0)return 0;
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-         if(t1.charAt(i)==t2.charAt(j)){
-            return 1+f(i-1,j-1,t1,t2,dp);
-         }
-         return dp[i][j]=Math.max(f(i-1,j,t1,t2,dp),f(i,j-1,t1,t2,dp));
+        if(s1.charAt(i)==s2.charAt(j)){
+            return 1+solve(i-1,j-1,s1,s2,dp);
+        }
+        return dp[i][j]= Math.max(solve(i,j-1,s1,s2,dp),solve(i-1,j,s1,s2,dp));
     }
-    public int lps(String s1,String s2){
+    public int lcs(String s1,String s2){
         int n=s1.length();
         int m=s2.length();
         int[][] dp=new int[n+1][m+1];
-        for(int[] rows:dp){
-            Arrays.fill(rows,-1);
+        for(int[] it:dp){
+            Arrays.fill(it,-1);
         }
-        return f(n-1,m-1,s1,s2,dp);
+        return solve(n-1,m-1,s1,s2,dp);
+
     }
     public int minInsertions(String s) {
         int n=s.length();
-        String t=new StringBuilder(s).reverse().toString();
-        int ans=lps(s,t);
-        return n-ans;
+          String rev=new StringBuilder(s).reverse().toString();
+        return n-lcs(s,rev);
     }
 }
