d3c7ee2c96ae5888ff9917f764fc5b1ebcea66bd
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-07-17 19:10:43 +0530

    Time: 2 ms (80.43%) | Memory: 46.1 MB (65.94%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,20 +1,19 @@
 class Solution {
-    private void swap(int[] nums,int t,int i){
-        int temp=nums[t];
-        nums[t]=nums[i];
-        nums[i]=temp;
+    public void swap(int[] nums,int a,int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
     }
     public void moveZeroes(int[] nums) {
-       
-     int lastNonZeroIndex=0;
-     int n=nums.length;
-    for(int i=0;i<n;i++){
-     if(nums[i]!=0){
-        swap(nums,lastNonZeroIndex,i);
-        lastNonZeroIndex++;
+        int lastNonZeroIndex=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums,lastNonZeroIndex,i);
+                lastNonZeroIndex++;
+            }
+        }
 
-     }
-    }
-
+        
     }
 }
