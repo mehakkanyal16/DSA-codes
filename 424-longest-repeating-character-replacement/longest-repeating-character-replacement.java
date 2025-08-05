1453b59906e136d7b7a2ffa16fe15771a90463fd
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-08-05 22:52:25 +0530

    Time: 6 ms (98.24%) | Memory: 44.8 MB (59.55%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,24 +1,21 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l = 0;
-        int max_len = 0;
-        int maxf = 0; // To keep track of the most frequent character in the window
-        int[] hash = new int[26];
+        int n=s.length();
+        int left=0;
+        int maxLen=0;
+        int maxfreq=0;
+        int[] hash=new int[126];
 
-        for (int r = 0; r < s.length(); r++) {
-            hash[s.charAt(r) - 'A']++; // Increment the frequency of the character at 'r'
-            maxf = Math.max(maxf, hash[s.charAt(r) - 'A']); // Update the max frequency
-
-            // If the window size minus the max frequency exceeds 'k', shrink the window
-            while ((r - l + 1) - maxf > k) {
-                hash[s.charAt(l) - 'A']--; // Decrease the frequency of the character at 'l'
-                l++; // Shrink the window from the left
+        for(int right=0;right<n;right++){
+            hash[s.charAt(right)-'A']++;
+            maxfreq=Math.max(hash[s.charAt(right)-'A'],maxfreq);
+            while((right-left+1)-maxfreq>k){
+                hash[s.charAt(left)-'A']--;
+                left++;
             }
+            maxLen=Math.max(right-left+1,maxLen);
 
-            // Update the maximum length of the window
-            max_len = Math.max(max_len, r - l + 1);
         }
-
-        return max_len;
+        return maxLen;
     }
 }
