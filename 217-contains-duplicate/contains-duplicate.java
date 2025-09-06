22de96f88797b21439e11db046a7ba2a92a5f8c2
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-09-06 19:04:33 +0530

    Time: 14 ms (61.94%) | Memory: 58.3 MB (53.51%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,47 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // HashMap<Integer,Integer>mp=new HashMap<>();
-        // for(int i=0;i<nums.length;i++){
-        //     mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-        // }
-        // for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
-        //     if(entry.getValue()>=2){
-        //         return true;
-        //     }
-        // }
-        // return false;
-
-        // Arrays.sort(nums);
-        // for(int i=1;i<nums.length;i++){
-        //     if(nums[i]==nums[i-1]){
-        //         return true;
-        //     }
-        // }
-        // return false;
-
-        //  HashSet<Integer>set=new HashSet<>();
-        //  for(int i=0;i<nums.length;i++){
-        //     set.add(nums[i]);
-        //  }
-        //  if(set.size()==nums.length){
-        //     return false;
-        //  }
-        //  return true;
-
-         HashSet<Integer>set=new HashSet<>();
-         for(int i=0;i<nums.length;i++){
-            if(set.contains(nums[i])){
-                return true;
-            }
-            set.add(nums[i]);
-         }
-         return false;
-
-
-
-
-
-
-        
+        HashSet<Integer>set=new HashSet<>();
+       for(int num:nums){
+        if(set.contains(num))return true;
+        set.add(num);
+       }
+       return false;
     }
 }
