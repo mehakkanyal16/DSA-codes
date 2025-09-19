181784c3f9b28da8a551b6e306aa7935088cc8a3
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-09-20 01:10:25 +0530

    Time: 2 ms (91.90%) | Memory: 41.3 MB (76.71%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,21 +1,24 @@
 class Solution {
     public String countAndSay(int n) {
-        if (n == 1) return "1";
-        return rle(countAndSay(n - 1));
+        if(n==1)return "1";
+        return rle(countAndSay(n-1));
+        
     }
+    public String rle(String s){
+        int count=1;
+        StringBuilder sb=new StringBuilder();
 
-    private String rle(String s) {
-        StringBuilder result = new StringBuilder();
-        int count = 1;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                count++;
-            } else {
-                result.append(count).append(s.charAt(i - 1));
-                count = 1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+               count++;
+            }else{
+                sb.append(count).append(s.charAt(i-1));
+                count=1;
             }
+
         }
-        result.append(count).append(s.charAt(s.length() - 1));
-        return result.toString();
+        sb.append(count).append(s.charAt(s.length()-1));
+        return sb.toString();
+
     }
 }
