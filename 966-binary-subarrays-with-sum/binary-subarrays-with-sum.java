eb8c4ef577985f07d77aab663f971dc8b48a33a5
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-09-25 01:36:26 +0530

    Time: 21 ms (40.82%) | Memory: 49.7 MB (21.69%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,25 +1,15 @@
 class Solution {
-    private int atMost(int[] nums, int goal) {
-        if (goal < 0) return 0;  // important edge case
-        int n = nums.length;
-        int cnt = 0;
-        int currSum = 0, left = 0;
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        HashMap<Integer,Integer>mp=new HashMap<>();
+        mp.put(0,1);
+        int prefix=0;
+        int cnt=0;
+        for(int num:nums){
+            prefix+=num;
+            cnt+=mp.getOrDefault(prefix-goal,0);
+            mp.put(prefix,mp.getOrDefault(prefix,0)+1);
 
-        for (int right = 0; right < n; right++) {
-            currSum += nums[right];
-
-            // ✅ Shrink window only if sum > goal
-            while (currSum > goal) {
-                currSum -= nums[left++];
-            }
-
-            // ✅ Count all subarrays ending at right
-            cnt += (right - left + 1);
         }
         return cnt;
-    }
-
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal - 1);
     }
 }
