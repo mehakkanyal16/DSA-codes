beec70890fda42874950d847b2b715c6d8e85520
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-09-25 00:30:11 +0530

    Time: 5 ms (88.81%) | Memory: 44.9 MB (43.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,17 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character,Integer>mp=new HashMap<>();
-        int left=0;
-        int maxLen=0;
+      
+       if(s.length()==0){
+        return 0;
+       }
+        HashMap<Character,Integer>mp=new HashMap<>(); //to store character & its index;
+        int maxCount=0; //count of longest substring 
+        int left=0 ;  //window pointers;
         for(int right=0;right<s.length();right++){
             char ch=s.charAt(right);
             if(mp.containsKey(ch)&&mp.get(ch)>=left){
                 left=mp.get(ch)+1;
             }
-            maxLen=Math.max(maxLen,right-left+1);
-            mp.put(ch,right);
-
+            maxCount=Math.max(maxCount,right-left+1);
+            mp.put(ch,right) ;
         }
-        return maxLen;
+        return maxCount;
+    
+
+        
     }
 }
