43a1622f675be257dcabe30e5fed4903cb338b2f
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-09-26 23:09:26 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (13.87%) - LeetSync

--- 390-elimination-game/elimination-game.java
+++ 390-elimination-game/elimination-game.java
@@ -1,14 +1,7 @@
 class Solution {
-    public int helper(int n,boolean leftToRight){
-        if(n==1)return 1;
-        if(leftToRight||n%2==1){
-            return 2*helper(n/2,!leftToRight);
-        }else{
-            return 2*helper(n/2,!leftToRight)-1;
-        }
-    }
+  
     public int lastRemaining(int n) {
-       return  helper(n,true);
+     return n == 1 ? 1 : (n / 2 - lastRemaining(n / 2) + 1) * 2;
 
     }
 }
