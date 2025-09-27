b43edb88569e50fbab719ae1aa946ccfdfa4358e
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-09-27 16:09:45 +0530

    Time: 1 ms (98.32%) | Memory: 44.3 MB (67.22%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,15 +15,20 @@
  */
 class Solution {
     int postIdx;
+    HashMap<Integer,Integer>mp=new HashMap<>();   //value->idx
+     
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         postIdx=postorder.length-1;
+       for(int i=0;i<inorder.length;i++){
+        mp.put(inorder[i],i);
+       }
         return helper(inorder,postorder,0,inorder.length-1);
         
     }
     public TreeNode helper(int[] inorder,int[] postorder ,int left,int right){
         if(left>right)return null;
         TreeNode root=new TreeNode(postorder[postIdx]);
-        int idx=search(inorder,left,right,postorder[postIdx]);
+        int idx=mp.get(postorder[postIdx]);
         postIdx--;
         root.right=helper(inorder,postorder,idx+1,right);
         root.left=helper(inorder,postorder,left,idx-1);
@@ -31,10 +36,5 @@ class Solution {
        
 
     } 
-    public int search(int[] inorder,int left,int right,int key){
-        for(int i=left;i<=right;i++){
-            if(inorder[i]==key)return i;
-        }
-        return -1;
-    }
+    
 }
