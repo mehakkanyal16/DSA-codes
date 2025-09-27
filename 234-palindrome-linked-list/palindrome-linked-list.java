ca469581b72c568f2b91d585f87c99ee1e803caa
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-09-27 20:41:30 +0530

    Time: 14 ms (13.64%) | Memory: 58.4 MB (81.24%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,71 +10,19 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        // ListNode curr=head;
-        // List<Integer>list=new ArrayList();
-        // while(curr!=null){
-        //     list.add(curr.val);
-        //     curr=curr.next;
-        // }
-        // int left=0;
-        // int right=list.size()-1;
-        // while(left<right&&list.get(left)==list.get(right)){
-        //     left++;
-        //     right--;
-        // }
-        // return left>=right;
-        
-        // time and space complexity of the above code is O(n).. so thats  why we can still optimise it more:
-
-
-     ListNode mid=Mid(head);
-     ListNode headSecond=reverse(mid);
-     ListNode RereverseHead=headSecond;
-
-
-     while(head!=null&&headSecond!=null){
-        if(head.val!=headSecond.val){
-            break;
-        }
-        head=head.next;
-        headSecond=headSecond.next;
-     }
-     reverse(RereverseHead);
-     if(head==null||headSecond==null){
-        return true;
-     }
-     return false;
-
-
-
-
-    }
-
-
-    public ListNode Mid(ListNode head){
-        ListNode s=head;
-        ListNode f=head;
-        while(f!=null&&f.next!=null){
-            s=s.next;
-            f=f.next.next;
-        }
-        return s;
-    }
-    public ListNode reverse(ListNode head){
-        if(head==null){
-            return head;
-        }
-        ListNode prev=null;
+        if(head==null)return false;
+        Stack<Integer>st=new Stack<>();
         ListNode curr=head;
-        ListNode next=curr.next;
         while(curr!=null){
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-            if(next!=null){
-                next=next.next;
-            }
+            st.push(curr.val);
+            curr=curr.next;
         }
-        return prev;
+        curr=head;
+        while(curr!=null&&curr.val==st.pop()){
+            curr=curr.next;
+        }
+        return curr==null;
+
+        
     }
 }
