c9e5aca5f79d3d934f2c528cf02f49d38946dfaf
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2026-07-02 00:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (51.32%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,5 +1,5 @@
 class Solution {
-    private void reverse(int[] row){
+        private void reverse(int[] row){
         int left=0;
         int right=row.length-1;
         while(left<right){
@@ -10,16 +10,25 @@ class Solution {
                 right--;
         }
     }
+
     public void rotate(int[][] matrix) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix[0].length;j++){
-                     int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        int n=matrix[0].length;
+        //transpose the matrix
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+
             }
         }
-        for(int i=0;i<matrix.length;i++){
+
+        //reverse each row :
+        for(int i=0;i<n;i++){
             reverse(matrix[i]);
+
         }
+
+        
     }
 }
