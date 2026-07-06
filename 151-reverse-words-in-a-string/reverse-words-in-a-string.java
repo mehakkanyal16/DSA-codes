728768164698958710c19b173cb1c63f064203ba
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2026-07-06 13:49:53 +0530

    Time: 7 ms (64.60%) | Memory: 44.1 MB (87.08%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,15 +1,14 @@
 class Solution {
     public String reverseWords(String s) {
+       
         StringBuilder sb=new StringBuilder();
         s=s.trim();
-        String[] arr=s.split("\\s+");
-    
+        String arr[]= s.split("\\s+");
         for(int i=arr.length-1;i>=0;i--){
             sb.append(arr[i]);
             if(i!=0){
              sb.append(" ");
            }
-   
         }
         return sb.toString();
         
