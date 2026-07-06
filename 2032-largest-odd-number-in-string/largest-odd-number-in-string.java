dc239c18e151852da767f8a9d3bc27ea5c4aec8b
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2026-07-06 14:06:22 +0530

    Time: 2 ms (17.36%) | Memory: 46.9 MB (89.05%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -1,14 +1,15 @@
 class Solution {
     public String largestOddNumber(String num) {
+        int n =num.length();
 
-        for(int i=num.length()-1;i>=0;i--){
-            char c=num.charAt(i);
-            int n=Character.getNumericValue(c);
-            if(n%2!=0){
+        for(int i=n-1;i>=0;i--){
+            char ch=num.charAt(i);
+            int nm=Character.getNumericValue(ch);
+            if(nm%2!=0){
                 return num.substring(0,i+1);
             }
-
         }
         return "";
+
     }
 }
