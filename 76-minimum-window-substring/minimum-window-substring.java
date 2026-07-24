a0c9f4993c54c3a074c18cd25d735a24aac8e96e
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2026-07-24 12:48:54 +0530

    Time: 3 ms (73.87%) | Memory: 45.3 MB (89.46%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,35 +1,41 @@
 class Solution {
     public String minWindow(String s, String t) {
+
+        if(s.length()<t.length()){
+            return "";
+        }
         int left=0;
         int right=0;
         int sIndex=-1;
-        int[] hash=new int[256];
-        int count=0;
+        int cnt=0;
         int min_len=Integer.MAX_VALUE;
+        int[] hash=new int[256];
         for(int i=0;i<t.length();i++){
-            hash[t.charAt(i)]++;
+             hash[t.charAt(i)]++;
         }
+
         while(right<s.length()){
             if(hash[s.charAt(right)]>0){
-                count++;
+                cnt++;
             }
             hash[s.charAt(right)]--;
-            while(count==t.length()){
-                if(right-left+1<min_len){
+
+            while(cnt==t.length()){
+                if((right-left+1)<min_len){
                     min_len=right-left+1;
                     sIndex=left;
-
                 }
                 hash[s.charAt(left)]++;
                 if(hash[s.charAt(left)]>0){
-                    count--;
+                    cnt--;
                 }
                 left++;
+
             }
             right++;
-
         }
-        return sIndex==-1?"":s.substring(sIndex,sIndex+min_len);
+
+        return (sIndex==-1)?"":s.substring(sIndex,sIndex+min_len);
         
     }
 }
